215fa54259ccc219378eb58db0ff5d086d093a9c
So Won Yim <sowon@ualberta.ca>
2021-02-15 15:02:19 -0700

    fixed conflict

--- Shape.java
+++ Shape.java
@@ -2,11 +2,9 @@ package com.example.teamgave;
 
 public abstract class Shape {
 	String color = "red";
-<<<<<<< HEAD
 	
-=======
 	// Code here
->>>>>>> 58cb0f8574bb9989df65f27f800888ba0fcaf375
+
 	public void Shape (int x, int y) {
 		// Code here
 	}
