22ea038ab3991c45180d5ff3005b1d9b27a53579
DoJeon1 <donghwan.dj@gmail.com>
2021-02-15 14:26:15 -0700

    shape

--- Shape.java
+++ Shape.java
@@ -1,11 +1,9 @@
 package com.example.teamgave;
 
 public abstract class Shape {
-	String color = "red";
+	String color = "yellow";
 	// Code here
-	String color = "yellow"
 	public void Shape (int x, int y) {
 		// Code here
 	}
-
 }
